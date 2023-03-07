3be5c30cbf31cdbe169de6a60064a96713c86ed9
bluethea <113011312+bluethea@users.noreply.github.com>
2023-03-07 06:21:06 +0200

    Add files via upload

---
+++ 1stTask.py
@@ -0,0 +1,31 @@
+# Task 1
+
+# Assign input values to variables using tuple unpacking
+n, a, b = input("Enter full text, text with positive characters, and text with negative characters separated by commas: ").split(",")
+
+def calculate_text_score(text, positive_chars, negative_chars):
+    # Check that all inputs are strings
+    if not isinstance(n, str) or not isinstance(a, str) or not isinstance(b, str):
+        raise TypeError("All inputs must be strings")
+    
+    # Check that all inputs are non-empty
+    if len(n) == 0 or len(a) == 0 or len(b) == 0:
+        raise ValueError("All inputs must be non-empty")
+    
+    # Initialize score to 0
+    score = 0
+    
+    # Loop through each character in n
+    for c in n:
+        # If the character is in a, increment score
+        if c in a:
+            score += 1
+        # If the character is in b, decrement score
+        elif c in b:
+            score -= 1
+    
+    # Return the calculated score
+    print(score)
+
+# Execute the function
+calculate_text_score(n, a, b)
---
+++ 5thTask.py
@@ -0,0 +1,46 @@
+# Task 5
+
+# Assign input value to a variable
+x = input("Enter a string: ")
+
+def compress_text(x):
+    # Check if the input is a string
+    if not isinstance(x, str):
+        raise TypeError("Input must be a string")
+    
+    # Check if the input string is empty
+    if len(x) == 0:
+        return ""
+    
+    # Initialize variables for the current character and its count
+    compressed_text = ""
+    current_char = x[0]
+    current_count = 1
+    
+    # Loop over the rest of the characters in the input string
+    for i in range(1, len(x)):
+        # If the current character is the same as the previous character,
+        # increment the count
+        if x[i] == current_char:
+            current_count += 1
+        # If the current character is different from the previous character,
+        # add the compressed version of the previous character and count to the
+        # output string, reset the current character and count to the current
+        # character and a count of 1
+        else:
+            compressed_text += current_char + str(current_count)
+            current_char = x[i]
+            current_count = 1
+    
+    # Add the compressed version of the last character and count to the output
+    # string
+    compressed_text += current_char + str(current_count)
+
+    # Return compressed output string
+    return compressed_text
+    
+# Calling function, providing params
+compressed = compress_text(x)
+
+# Returning compressed string value
+print(compressed)
