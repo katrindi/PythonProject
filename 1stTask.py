09689faf740b09876384cbbf1dd0b5cd6fa1774f
katrindi <107497899+katrindi@users.noreply.github.com>
2023-03-08 09:51:24 +0200

    Update 1stTask.py

--- 1stTask.py
+++ 1stTask.py
@@ -1,20 +1,23 @@
 # -- TASK 1 -- #
 
 # Assign input values to variables using tuple unpacking
-n, a, b = input("Enter full text, text with positive characters, and text with negative characters separated by commas: ").split(",")
+n, a, b = input(
+    "Enter full text, text with positive characters, and text with negative characters separated by commas: ").split(
+    ",")
+
 
 def calculate_text_score(text, negative_chars, positive_chars):
     # Check that all inputs are strings
     if not isinstance(text, str) or not isinstance(positive_chars, str) or not isinstance(negative_chars, str):
         raise TypeError("All inputs must be strings")
-    
+
     # Check that all inputs are non-empty
     if len(text) == 0 or len(positive_chars) == 0 or len(negative_chars) == 0:
         raise ValueError("All inputs must be non-empty")
-    
+
     # Initialize score to 0
     score = 0
-    
+
     # Loop through each character in n
     for c in text:
         # If the character is in a (positive chars), increment score
@@ -23,9 +26,10 @@ def calculate_text_score(text, negative_chars, positive_chars):
         # If the character is in b (negative chars), decrease score
         elif c in negative_chars:
             score -= 1
-    
+
     # Return the calculated score
     print(score)
+
 
 # Execute the function
 calculate_text_score(n, a, b)
